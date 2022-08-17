a2820922fc92d38f8d30217c0faf25050c6a75d0
Saipraneeth M <2506664+msaipraneeth@users.noreply.github.com>
2022-08-17 19:33:55 +0530

    add needs_cmem annotation and Test Context classes

---
+++ src/tests/utils.py
@@ -0,0 +1,37 @@
+"""Testing utilities."""
+import os
+from typing import Optional
+
+import pytest
+
+# check for cmem environment and skip if not present
+from cmem.cmempy.api import get_token
+
+from cmem_plugin_base.dataintegration.context import PluginContext, UserContext
+
+needs_cmem = pytest.mark.skipif(
+    "CMEM_BASE_URI" not in os.environ, reason="Needs CMEM configuration"
+)
+
+
+class TestUserContext(UserContext):
+    """dummy user context that can be used in tests"""
+
+    __test__ = False
+
+    def __init__(self):
+        # get access token from default service account
+        access_token: str = get_token()["access_token"]
+        self.token = lambda: access_token
+
+
+class TestPluginContext(PluginContext):
+    """dummy plugin context that can be used in tests"""
+
+    __test__ = False
+
+    def __init__(self, project_id: str = "dummyProject",
+                 user: Optional[UserContext] = TestUserContext()):
+        self.project_id = project_id
+        self.user = user
+
